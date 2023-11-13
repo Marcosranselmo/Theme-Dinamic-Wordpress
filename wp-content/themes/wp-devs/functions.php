5354ef362a261b57cb419c75a01095f6769f0ded
Marcos Roberto Anselmo <marcos.anselmo@hotmail.com>
2023-11-12 21:53:43 -0300

    image-thumbnail

--- wp-content/themes/wp-devs/functions.php
+++ wp-content/themes/wp-devs/functions.php
@@ -20,5 +20,6 @@ function wpdevs_config(){
         'width'  => 1920
     );
     add_theme_support( 'custom-header', $args );
+    add_theme_support( 'post-thumbnails' );
 }
 add_action( 'after_setup_theme', 'wpdevs_config', 0 );
--- wp-content/themes/wp-devs/index.php
+++ wp-content/themes/wp-devs/index.php
@@ -15,6 +15,7 @@ width="<?php echo get_custom_header()->width; ?>" alt="" />
                                     ?>
                                         <article>
                                             <h2><?php the_title(); ?></h2>
+                                            <?php the_post_thumbnail( array( 275, 275 ) ); ?>
                                             <div class="meta-info">
                                                 <p>Posted in <?php echo get_the_date(); ?> by <?php the_author_posts_link(); ?></p>
                                                 <p>Categories: <?php the_category( ' ' ); ?></p>
