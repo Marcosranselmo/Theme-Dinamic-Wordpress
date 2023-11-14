4543ba168f70cf03b6ccb7615b97efa17bc78fc5
Marcos Roberto Anselmo <marcos.anselmo@hotmail.com>
2023-11-14 11:29:29 -0300

    classes-wp_query

--- wp-content/themes/wp-devs/page-home.php
+++ wp-content/themes/wp-devs/page-home.php
@@ -32,27 +32,41 @@
                         </div>
                     </section>
                     <section class="home-blog">
+                        <h2>Latest News</h2>
                         <div class="container">
-                            <div class="blog-items">
-                                <?php 
-                                    if( have_posts() ):
-                                        while( have_posts() ) : the_post();
-                                        ?>
-                                            <article>
-                                                <h2><?php the_title(); ?></h2>
-                                                <div class="meta-info">
-                                                    <p>Posted in <?php echo get_the_date(); ?> by <?php the_author_posts_link(); ?></p>
-                                                    <p>Categories: <?php the_category( ' ' ); ?></p>
-                                                    <p>Tags: <?php the_tags( '', ', '); ?></p>
-                                                </div>
-                                                <?php the_content(); ?>
-                                            </article>
-                                        <?php
-                                        endwhile;
-                                    else: ?>
-                                        <p>Nothing yet to be displayed!</p>
-                                <?php endif; ?>                                
-                            </div>
+                            <?php 
+
+                            $args = array(
+                                'post_type' => 'post',
+                                'posts_per_page' => 5,
+                                'category__in' => array( 4, 5, 6 ),
+                                'category__not_in' => array( 1 )
+                            );        
+
+                            $postlist = new WP_Query( $args );
+
+                                if( $postlist->have_posts() ):
+                                    while( $postlist->have_posts() ) : $postlist->the_post();
+                                    ?>
+                                    <article class="lastet-news">
+                                        <?php the_post_thumbnail( 'large' ); ?>
+                                        <h3><?php the_title(); ?></h3>
+                                        <div class="meta-info">
+                                            <p>
+                                                by <span><?php the_author_posts_link(); ?></span> 
+                                                Categories: <span><?php the_category( ' ' ); ?></span>
+                                                Tags: <?php the_tags( '', ', '); ?>
+                                            </p>
+                                            <p><span><?php echo get_the_date(); ?></span></p>
+                                        </div>
+                                        <?php the_excerpt(); ?>
+                                    </article>
+                                    <?php
+                                    endwhile;
+                                    wp_reset_postdata();
+                                else: ?>
+                                    <p>Nothing yet to be displayed!</p>
+                            <?php endif; ?>                                
                         </div>
                     </section>
                 </main>
