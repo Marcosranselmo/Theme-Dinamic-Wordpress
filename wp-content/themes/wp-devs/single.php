136da30e0324c23ecfaba5362043462e457430d2
Marcos Roberto Anselmo <marcos.anselmo@hotmail.com>
2023-11-14 17:23:56 -0300

    single-post

---
+++ wp-content/themes/wp-devs/single.php
@@ -0,0 +1,31 @@
+<?php get_header(); ?>
+
+<div id="primary">
+    <div id="main">
+        <div class="container">
+
+            <?php
+            while( have_posts() ): the_post();
+            ?>
+            <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+
+                <header>
+                    <h1><?php the_title(); ?></h1>
+                    <div class="meta-info">
+                        <p>Posted in <?php echo get_the_date(); ?>by</p>
+                        <p>Categories: <?php the_category( ' ' ); ?></p>
+                        <p>Tags: <?php the_tags( '', ', ' ); ?></p>
+                    </div>
+                </header>
+                <div class="content">
+                    <?php the_content(); ?>
+                </div>
+            </article>
+            <?php
+            endwhile;
+            ?>
+        </div>
+    </div>
+</div>
+
+<?php get_footer(); ?>
