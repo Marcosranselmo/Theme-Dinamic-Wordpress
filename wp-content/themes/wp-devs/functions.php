8618407a60f363a55d45e22d646b1bc8c2482a01
Marcos Roberto Anselmo <marcos.anselmo@hotmail.com>
2023-11-17 19:08:08 -0300

    Internacionalizacao-theme2

--- wp-content/themes/wp-devs/functions.php
+++ wp-content/themes/wp-devs/functions.php
@@ -22,74 +22,73 @@ function wpdevs_config(){
     );
 
     $args = array(
-        'height' => 225,
-        'width'  => 1920
+        'height'    => 225,
+        'width'     => 1920
     );
     add_theme_support( 'custom-header', $args );
     add_theme_support( 'post-thumbnails' );
     add_theme_support( 'custom-logo', array(
-    'width' => 200,
-    'height' => 50,
-    'flex-height' => true,
-    'flex-width' => true
+        'width' => 200,
+        'height'    => 110,
+        'flex-height'   => true,
+        'flex-width'    => true
     ) );
     add_theme_support( 'automatic-feed-links' );
-    add_theme_support( 'html5', array( 'comment', 'comment-form', 'search-form',
-    'gallery', 'caption', 'style', 'script' ));
+    add_theme_support( 'html5', array( 'comment-list', 'comment-form', 'search-form', 'gallery', 'caption', 'style', 'script' ));
     add_theme_support( 'title-tag' );
 }
 add_action( 'after_setup_theme', 'wpdevs_config', 0 );
 
-add_action( 'widgets_init', 'wpdevs_sidebars');
-function wpdevs_sidebars() {
+add_action( 'widgets_init', 'wpdevs_sidebars' );
+function wpdevs_sidebars(){
     register_sidebar(
         array(
-            'name' => __( 'Blog Sidebar', 'wp-devs' ),
-            'id'   => 'sidebar-blog',
+            'name'  => __( 'Blog Sidebar', 'wp-devs' ),
+            'id'    => 'sidebar-blog',
             'description'   => __( 'This is the Blog Sidebar. You can add your widgets here.', 'wp-devs' ),
             'before_widget' => '<div class="widget-wrapper">',
             'after_widget'  => '</div>',
             'before_title'  => '<h4 class="widget-title">',
-            'after_title'   => '</h4>'        
+            'after_title'   => '</h4>'
         )
     );
     register_sidebar(
         array(
-            'name' => __( 'Service 1', 'wp-devs' ),
-            'id'   => 'services-1',
-            'description'   => __( 'First Service Area.', 'wp-devs' ),
+            'name'  => __( 'Service 1', 'wp-devs' ),
+            'id'    => 'services-1',
+            'description'   => __( 'First Service Area', 'wp-devs' ),
             'before_widget' => '<div class="widget-wrapper">',
             'after_widget'  => '</div>',
             'before_title'  => '<h4 class="widget-title">',
-            'after_title'   => '</h4>'        
+            'after_title'   => '</h4>'
         )
     );
     register_sidebar(
         array(
-            'name' => __( 'Service 2', 'wp-devs' ),
-            'id'   => 'services-2',
-            'description'   => __( 'Secund Service Area.', 'wp-devs' ),
+            'name'  => __( 'Service 2', 'wp-devs' ),
+            'id'    => 'services-2',
+            'description'   => __( 'Second Service Area', 'wp-devs' ),
             'before_widget' => '<div class="widget-wrapper">',
             'after_widget'  => '</div>',
             'before_title'  => '<h4 class="widget-title">',
-            'after_title'   => '</h4>'        
+            'after_title'   => '</h4>'
         )
     );
     register_sidebar(
         array(
-            'name' => __( 'Service 3', 'wp-devs' ),
-            'id'   => 'services-3',
-            'description'   => __( 'Third Service Area.', 'wp-devs' ),
+            'name'  => __( 'Service 3', 'wp-devs' ),
+            'id'    => 'services-3',
+            'description'   => __( 'Third Service Area', 'wp-devs' ),
             'before_widget' => '<div class="widget-wrapper">',
             'after_widget'  => '</div>',
             'before_title'  => '<h4 class="widget-title">',
-            'after_title'   => '</h4>'        
+            'after_title'   => '</h4>'
         )
     );
 }
 
-if ( ! function_exists( 'wp_body_open' ) ) {
-    function wp_body_open() {
-        do_action( 'wp_body_open' );
-    }
+if ( ! function_exists( 'wp_body_open' ) ){
+	function wp_body_open() {
+		do_action( 'wp_body_open' );
+	}
 }
