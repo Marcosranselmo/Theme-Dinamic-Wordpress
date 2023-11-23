7cb9df3917ed61e030682b5dfadc803ef29e1250
Marcos Roberto Anselmo <marcos.anselmo@hotmail.com>
2023-11-23 16:51:44 -0300

    Page-pedras-granitos-marmores01

--- wp-content/themes/wp-devs/page-home.php
+++ wp-content/themes/wp-devs/page-home.php
@@ -47,44 +47,44 @@
 
                         ?>
 
-                                <li>
+                            <li>
 
-                                    <div class="slider-container">
-                                        <?php
-                                        // If we have a thumbnail, show it
-                                        // If not, we show a placeholder image
-                                        if (has_post_thumbnail()) :
-                                        ?>
+                                <div class="slider-container">
+                                    <?php
+                                    // If we have a thumbnail, show it
+                                    // If not, we show a placeholder image
+                                    if (has_post_thumbnail()) :
+                                    ?>
 
-                                            <figure>
-                                                <?php the_post_thumbnail('large'); ?>
-                                            </figure>
+                                        <figure>
+                                            <?php the_post_thumbnail('large'); ?>
+                                        </figure>
 
-                                        <?php else : ?>
-                                            <img class="img-responsive" src="https://unsplash.it/1920/650/?image=<?php echo $cont ?>" title="<?php echo esc_attr_x('Placeholder Image', 'title', 'parea'); ?>">
-                                        <?php endif; ?>
+                                    <?php else : ?>
+                                        <img class="img-responsive" src="https://unsplash.it/1920/650/?image=<?php echo $cont ?>" title="<?php echo esc_attr_x('Placeholder Image', 'title', 'parea'); ?>">
+                                    <?php endif; ?>
 
-                                        <div class="container">
-                                            <div class="slider-details-container">
+                                    <div class="container">
+                                        <div class="slider-details-container">
 
-                                                <div class="slider-title">
-                                                    <h3><a href="<?php the_permalink(); ?>" class="slider-title"><?php the_title(); ?></a></h3>
-                                                </div>
-
-                                                <div class="slider-description">
-                                                    <?php the_excerpt(); ?>
-                                                </div>
-
-                                                <div class="slider-readmore-button">
-                                                    <a href="<?php the_permalink(); ?>" class="btn"><?php echo _e('Read More!', 'parea') ?></a>
-                                                </div>
-
+                                            <div class="slider-title">
+                                                <h3><a href="<?php the_permalink(); ?>" class="slider-title"><?php the_title(); ?></a></h3>
                                             </div>
-                                        </div>
 
+                                            <div class="slider-description">
+                                                <?php the_excerpt(); ?>
+                                            </div>
+
+                                            <div class="slider-readmore-button">
+                                                <a href="<?php the_permalink(); ?>" class="btn"><?php echo _e('Read More!', 'parea') ?></a>
+                                            </div>
+
+                                        </div>
                                     </div>
 
-                                </li>
+                                </div>
+
+                            </li>
 
                         <?php
                                 $cont++;
