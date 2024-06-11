5895b600bd123db2e2057afdd82b83b6f9e16e9d
Marcos Roberto Anselmo <marcos.anselmo@hotmail.com>
2024-06-11 05:49:43 -0300

    conteudo-informacoes

--- wp-content/themes/wp-devs/footer.php
+++ wp-content/themes/wp-devs/footer.php
@@ -1,5 +1,10 @@
 <div class="site-rodape">
     <div class="container">
+        <div class="endereco1">
+        <h6>Endereço1</h6>
+            <p><?php echo esc_html(get_theme_mod('set_endereco', __('Configure o endereço da empresa', 
+            'wp-devs'))); ?></p>
+        </div>
         <div class="endereco">
         <h6>Endereço</h6>
             <p><?php echo esc_html(get_theme_mod('set_endereco', __('Configure o endereço da empresa', 
