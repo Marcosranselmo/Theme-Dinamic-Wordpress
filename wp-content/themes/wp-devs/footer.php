662dbc75e8ceb048046d41f0a02a7bfd068fa510
Marcos Roberto Anselmo <marcos.anselmo@hotmail.com>
2024-06-14 05:20:34 -0300

    conteudo-fale-conosco

--- wp-content/themes/wp-devs/footer.php
+++ wp-content/themes/wp-devs/footer.php
@@ -4,6 +4,7 @@
     <div class="container">
         <div class="localizacao">
         <h6>Localização</h6>
+        <!-- <a href=""><img src="./img/icone-localização.png" alt=""></a> -->
             <p><?php echo esc_html(get_theme_mod('set_localizacao', __('Configure o endereço da empresa', 
             'wp-devs'))); ?></p>
         </div>
@@ -14,6 +15,9 @@
         </div>
     </div>
 </div>
+
+<div class="fale-conosco">Fale Conosco</div>
+
 <footer class="site-footer">
     <div class="container">
         <div class="copyright">
