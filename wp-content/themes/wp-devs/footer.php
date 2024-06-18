8a081c92fe606da5bc168cfaa417244dde65aef7
Marcos Roberto Anselmo <marcos.anselmo@hotmail.com>
2024-06-18 05:20:43 -0300

    conteudo-informacoes

--- wp-content/themes/wp-devs/footer.php
+++ wp-content/themes/wp-devs/footer.php
@@ -1,44 +1,46 @@
 <!-- Informações -->
 
-<div class="informacoes">Informações</div>
+<div class="informacoes">Informações
 
-<div class="site-rodape">
-    <div class="container">
-        <div class="localizacao">
-        <h6>Localização</h6>
-        <!-- <a href=""><img src="./img/icone-localização.png" alt=""></a> -->
-            <p><?php echo esc_html(get_theme_mod('set_localizacao', __('Configure o endereço da empresa', 
-            'wp-devs'))); ?></p>
-        </div>
-        <div class="horario-atendimento">
-        <h6>Horário Atendimento</h6>
-            <p><?php echo esc_html(get_theme_mod('set_horario-atendimento', __('Configure o horário de atendimento da empresa', 
-            'wp-devs'))); ?></p>
+    <div class="site-rodape">
+        <div class="container">
+            <div class="localizacao">
+            <h6>Localização</h6>
+            <!-- <a href=""><img src="./img/icone-localização.png" alt=""></a> -->
+                <p><?php echo esc_html(get_theme_mod('set_localizacao', __('Configure o endereço da empresa', 
+                'wp-devs'))); ?></p>
+            </div>
+            <div class="horario-atendimento">
+            <h6>Horário Atendimento</h6>
+                <p><?php echo esc_html(get_theme_mod('set_horario-atendimento', __('Configure o horário de atendimento da empresa', 
+                'wp-devs'))); ?></p>
+            </div>
         </div>
     </div>
 </div>
 
 <!-- Fale Conosco -->
 
-<div class="informacoes">Fale conosco</div>
+<div class="fale-conosco">Fale conosco
 
-<div class="site-rodape">
-    <div class="container">
-        <div class="tel-fixo">
-        <h6>Telefone Fixo</h6>
-        <!-- <a href=""><img src="./img/icone-localização.png" alt=""></a> -->
-            <p><?php echo esc_html(get_theme_mod('set_tel-fixo', __('Configure o tel. fixo da empresa', 
-            'wp-devs'))); ?></p>
-        </div>
-        <div class="whatsapp">
-        <h6>WhatsApp</h6>
-            <p><?php echo esc_html(get_theme_mod('set_whatsapp', __('Configure o whasapp da empresa', 
-            'wp-devs'))); ?></p>
-        </div>
-        <div class="email">
-        <h6>E-mail</h6>
-            <p><?php echo esc_html(get_theme_mod('set_email', __('Configure o email da empresa', 
-            'wp-devs'))); ?></p>
+    <div class="site-rodape">
+        <div class="container">
+            <div class="tel-fixo">
+            <h6>Telefone Fixo</h6>
+            <!-- <a href=""><img src="./img/icone-localização.png" alt=""></a> -->
+                <p><?php echo esc_html(get_theme_mod('set_tel-fixo', __('Configure o tel. fixo da empresa', 
+                'wp-devs'))); ?></p>
+            </div>
+            <div class="whatsapp">
+            <h6>WhatsApp</h6>
+                <p><?php echo esc_html(get_theme_mod('set_whatsapp', __('Configure o whasapp da empresa', 
+                'wp-devs'))); ?></p>
+            </div>
+            <div class="email">
+            <h6>E-mail</h6>
+                <p><?php echo esc_html(get_theme_mod('set_email', __('Configure o email da empresa', 
+                'wp-devs'))); ?></p>
+            </div>
         </div>
     </div>
 </div>
